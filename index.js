16a3760cab7b9c15c22de44476fa4b15048c5c86
Minin <70429604+MininxD@users.noreply.github.com>
2023-12-20 19:50:01 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var fs = require('fs');
 var path = require('path');
-var port = 3000
+var port = 8080
 
 http.createServer(function (request, response) {
     console.log('request ', request.url);
