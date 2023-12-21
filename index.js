5c3612130051676b50ff8e6797c3581a988b99c5
Minin <70429604+MininxD@users.noreply.github.com>
2023-12-21 14:24:39 +0700

    express js bangsat

--- index.js
+++ index.js
@@ -1,19 +1,56 @@
-const express = require('express')
-const path = require('path')
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
+var port = 3000
 
-const app = express()
-const PORT = 8800
+http.createServer(function (request, response) {
+    console.log('request ', request.url);
 
+    var filePath = '.' + request.url;
+    if (filePath == './') {
+        filePath = './home.html';
+    }
 
-app.use(express.static('./'))
-app.use(express.static(__dirname))
-app.listen(PORT, () => {
-  console.log(`API listening on PORT ${PORT} `)
-})
+    var extname = String(path.extname(filePath)).toLowerCase();
+    var mimeTypes = {
+        '.html': 'text/html',
+        '.js': 'text/javascript',
+        '.css': 'text/css',
+        '.json': 'application/json',
+        '.png': 'image/png',
+        '.jpg': 'image/jpg',
+        '.gif': 'image/gif',
+        '.svg': 'image/svg+xml',
+        '.wav': 'audio/wav',
+        '.mp4': 'video/mp4',
+        '.woff': 'application/font-woff',
+        '.ttf': 'application/font-ttf',
+        '.eot': 'application/vnd.ms-fontobject',
+        '.otf': 'application/font-otf',
+        '.wasm': 'application/wasm'
+    };
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/home.html'));
-});
+    var contentType = mimeTypes[extname] || 'application/octet-stream';
 
-// Export the Express API
-module.exports = app
+    fs.readFile(filePath, function(error, content) {
+        if (error) {
+            if(error.code == 'ENOENT') {
+                fs.readFile('./404.html', function(error, content) {
+                    response.writeHead(404, { 'Content-Type': 'text/html' });
+                    response.end(content, 'utf-8');
+                });
+            }
+            else {
+                response.writeHead(500);
+                response.end('Sorry, web application error: '+error.code+' ..\n');
+            }
+        }
+        else {
+            response.writeHead(200, { 'Content-Type': contentType });
+            response.end(content, 'utf-8');
+        }
+    });
+
+}).listen(port);
+
+console.log('Server running at http://127.0.0.1:'+port);
