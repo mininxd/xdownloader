162539196336ccef30f1a495d9a8ac45bd8f9321
Minin <70429604+MininxD@users.noreply.github.com>
2023-12-22 20:52:17 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, './home.html'));
+  res.sendFile(path.join(__dirname, 'home.html'));
 });
 
 app.listen(port, () => {
