b4336b969dfc237a19ab7e5c789d96d9c8a7a721
Minin <70429604+MininxD@users.noreply.github.com>
2023-12-23 08:56:48 +0700

    Update placeholder.js

--- js/placeholder.js
+++ js/placeholder.js
@@ -14,6 +14,8 @@ var pHolder = [
 'Hapus Aku - NIDJI',
 'bmth - parasite eve',
 'jvke - golden hour',
+'Tabun (多分)',
+'怪物 (Kaibutsu)',
 ]
 var rand = Math.floor(Math.random() * pHolder.length);
 document.getElementById("inputQueryEl").placeholder = pHolder[rand];
